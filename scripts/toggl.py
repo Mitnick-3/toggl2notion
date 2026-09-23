7171a16c068911cb3c2cfc54b7d946a82aa4433e
Mitnick-3 <53416267+Mitnick-3@users.noreply.github.com>
2026-09-23 08:29:46 +0800

    Update toggl.py

--- scripts/toggl.py
+++ scripts/toggl.py
@@ -18,6 +18,7 @@ def get_time_entries():
     start = now.subtract(days=30).start_of("day")
     # 格式化时间
     start = start.to_iso8601_string()
+    print("start")
     params = {"start_date": start, "end_date": end}
     response = requests.get(
         "https://api.track.toggl.com/api/v9/me/time_entries", params=params, auth=auth
@@ -34,6 +35,7 @@ def insert_to_notion():
     start = now.subtract(days=30)
     # 格式化时间
     start = start.to_iso8601_string()
+    print("start")
     sorts = [{"property": "时间", "direction": "descending"}]
     page_size = 1
     response = notion_helper.query(
