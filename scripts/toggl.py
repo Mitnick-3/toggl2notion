15bc890a82ef8ef70e0e4fea593d9b2c5375f8a4
Mitnick-3 <53416267+Mitnick-3@users.noreply.github.com>
2026-09-23 08:46:05 +0800

    Update toggl.py

--- scripts/toggl.py
+++ scripts/toggl.py
@@ -32,12 +32,10 @@ def insert_to_notion():
     now = pendulum.now("Asia/Shanghai")
     # toggl只支持90天的数据
     end = now.to_iso8601_string()
-    print(now)
     start = now.subtract(days=30)
     # 格式化时间
-    print(start)
     start = start.to_iso8601_string()
-    print(end)
+    print(start)
     sorts = [{"property": "时间", "direction": "descending"}]
     page_size = 1
     response = notion_helper.query(
